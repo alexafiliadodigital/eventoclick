d8720bda2d51a50ed7be10e7516c6dc031c3c2eb
alexafiliadodigital <108899496+alexafiliadodigital@users.noreply.github.com>
2023-08-19 11:54:40 -0300

    commit primeiro

---
+++ arquivo.js
@@ -0,0 +1,42 @@
+
+     
+
+
+   
+
+      
+
+     addEventListener("click",clicar)
+     addEventListener("mouseout",sair)
+
+     
+ 
+
+    function clicar(){
+ 
+     var estilo= document.querySelector(".quadrado-estiloso")
+
+
+     estilo.style.backgroundColor="green"
+
+   }
+
+
+ 
+
+
+    function sair(){
+
+    var tinta=document.querySelector(".quadrado-estiloso")
+    tinta.style.backgroundColor="yellow"
+   
+
+ }
+
+ 
+
+
+
+
+
+
